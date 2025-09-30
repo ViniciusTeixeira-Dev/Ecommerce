5eeca427e9627d125e45a1123d7504d5925b15d2
ViniciusTeixeira-Dev <contato.viniciusaraujoteixeira@gmail.com>
2025-09-30 19:55:40 -0300

    refactor(ItensPedidoDTO): Simplifica nomes de atributos.

--- src/main/java/br/com/ecommerce/dto/ItensPedidoDTO.java
+++ src/main/java/br/com/ecommerce/dto/ItensPedidoDTO.java
@@ -3,16 +3,7 @@ package br.com.ecommerce.dto;
 public class ItensPedidoDTO {
 
     private Integer idProduto;
-    private String nomeProduto;
     private Integer quantidade;
-    private Double valorUnitarioVenda;
-    private Double valorTotalItem;
-
-
-
-
-
-
 
 
     public Integer getIdProduto() {
@@ -23,35 +14,11 @@ public class ItensPedidoDTO {
         this.idProduto = idProduto;
     }
 
-    public String getNomeProduto() {
-        return nomeProduto;
-    }
-
-    public void setNomeProduto(String nomeProduto) {
-        this.nomeProduto = nomeProduto;
-    }
-
     public Integer getQuantidade() {
         return quantidade;
     }
 
     public void setQuantidade(Integer quantidade) {
         this.quantidade = quantidade;
-    }
-
-    public Double getValorUnitarioVenda() {
-        return valorUnitarioVenda;
-    }
-
-    public void setValorUnitarioVenda(Double valorUnitarioVenda) {
-        this.valorUnitarioVenda = valorUnitarioVenda;
-    }
-
-    public Double getValorTotalItem() {
-        return valorTotalItem;
-    }
-
-    public void setValorTotalItem(Double valorTotalItem) {
-        this.valorTotalItem = valorTotalItem;
     }
 }
